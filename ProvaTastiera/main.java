b5465545764b04845357ee478971b5c4778974a9
RaffaeleMelluso <raffaelemelluso01@gmail.com>
2023-05-08 10:55:13 +0200

    lezione 8L

--- ProvaTastiera/main.java
+++ ProvaTastiera/main.java
@@ -3,7 +3,7 @@ public class main {
     public static void main(String [] args)
     {
         Persona p1 = new Persona(true);
-        System.out.println(p1.toString);
+        System.out.println(p1.toString());
 
     }
 }
